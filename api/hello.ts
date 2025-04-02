38d6c8bae5a8087b889f4050422df7f706ea46a4
Pradise2 <akinsunmitaye@gmail.com>
2025-04-02 10:41:08 +0100

    Your commit message

---
+++ api/hello.ts
@@ -0,0 +1,11 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default function handler(
+  request: VercelRequest,
+  response: VercelResponse
+) {
+  response.status(200).json({
+    message: "Hello from TypeScript!",
+    query: request.query,
+  });
+}
